ed3aba658b714aee6acd1732012a69bc3ac9c070
alvin000009238 <107313913+alvin000009238@users.noreply.github.com>
2025-06-07 16:13:24 +0800

    Delete api directory

--- api/log-ip.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  console.log("訪客 IP：", ip);
-
-  res.status(200).json({ message: "IP logged", ip });
-}
